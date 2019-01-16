465d26af3cd49e00abbf25a314a2d6990bd2c01f
shellcreasy <43395757+shellcreasy@users.noreply.github.com>
2019-01-16 22:29:15 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+message = "hello python world"
+print(message)
+
+message = "hello python crash course world"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+firstname = "feng"
+lastname = "xueyao"
+print(firstname+"   "+lastname)
